246f2f959576e02fb9cf0c69ccf2536e998dddaf
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-03-30 22:31:15 +0800

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,32 @@
+import os
+from subprocess import Popen
+
+
+all_command = []
+
+for file, size, p, t in [
+    ("./img/dragon-girl.png", 256, 6, 1),
+    ("./img/house.png", 128, 8, 2),
+    ("./img/horse-girl.png", 256, 8, 2),
+]:
+    output_file = os.path.join("./demo", os.path.splitext(os.path.basename(file))[0])
+    for thickness in [0, t]:
+        for mode in ["nearest", "bicubic", "center", "k-centroid", "contrast"]:
+            all_command.append(
+                "python -m pixeloe.cli "
+                f"{file} --thickness {thickness} "
+                f"--target_size {size} --patch_size {p} "
+                f"-M {mode} -O {output_file}-t{thickness}-{mode}.png"
+            )
+
+
+## Use Popen to run all commands in parallel
+all_process = []
+for command in all_command:
+    print(command)
+    all_process.append(Popen(command, shell=True))
+
+
+## Wait for all processes to finish
+for process in all_process:
+    process.wait()
