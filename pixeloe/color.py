f210244b3995379edfd25215500f9d7bae903173
KohakuBlueleaf <apolloyeh0123@gmail.com>
2024-04-06 22:44:39 +0900

    better naming to avoid conflict with built-in

--- pixeloe/color.py
+++ pixeloe/color.py
@@ -22,31 +22,31 @@ def match_color(source, target):
     return output.clip(0, 255).astype(np.uint8)
 
 
-def wavelet_colorfix(input, target):
-    input_high, _ = wavelet_decomposition(input, 5)
+def wavelet_colorfix(inp, target):
+    inp_high, _ = wavelet_decomposition(inp, 5)
     _, target_low = wavelet_decomposition(target, 5)
-    output = input_high + target_low
+    output = inp_high + target_low
     return output
 
 
-def wavelet_decomposition(input, levels):
-    high_freq = np.zeros_like(input)
+def wavelet_decomposition(inp, levels):
+    high_freq = np.zeros_like(inp)
     for i in range(1, levels + 1):
         radius = 2**i
-        low_freq = wavelet_blur(input, radius)
-        high_freq = high_freq + (input - low_freq)
-        input = low_freq
+        low_freq = wavelet_blur(inp, radius)
+        high_freq = high_freq + (inp - low_freq)
+        inp = low_freq
     return high_freq, low_freq
 
 
-def wavelet_blur(input, radius):
+def wavelet_blur(inp, radius):
     kernel_size = 2 * radius + 1
-    output = cv2.GaussianBlur(input, (kernel_size, kernel_size), 0)
+    output = cv2.GaussianBlur(inp, (kernel_size, kernel_size), 0)
     return output
 
 
-def color_styling(input, saturation=1.2, contrast=1.1):
-    output = input.copy()
+def color_styling(inp, saturation=1.2, contrast=1.1):
+    output = inp.copy()
     output = cv2.cvtColor(output, cv2.COLOR_BGR2HSV)
     output[:, :, 1] = output[:, :, 1] * saturation
     output[:, :, 2] = output[:, :, 2] * contrast - (contrast - 1)
@@ -55,8 +55,8 @@ def color_styling(input, saturation=1.2, contrast=1.1):
     return output
 
 
-def kmeans_color_quant(input, colors=32):
-    img = cv2.cvtColor(input, cv2.COLOR_BGR2RGB)
+def kmeans_color_quant(inp, colors=32):
+    img = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
     img_pil = Image.fromarray(img)
     img_quant = img_pil.quantize(colors, 1, kmeans=colors).convert("RGB")
     return cv2.cvtColor(np.array(img_quant), cv2.COLOR_RGB2BGR)
